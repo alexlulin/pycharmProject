db073afedfb64b502dedc84f5565ebe08f09f964
alexlulin232100 <7986202+alexlulin232100@user.noreply.gitee.com>
2025-01-16 15:50:02 +0800

    第二次commit

--- myproject/comdepart/views.py
+++ myproject/comdepart/views.py
@@ -1,6 +1,14 @@
 from django.shortcuts import render
-
+from comdepart import models
 # Create your views here.
 def depart_list(request):
     """部门列表"""
-    return render(request,"depart_list.html")
+    """从数据库中获取数据"""
+    queryset=models.Department.objects.all()
+    return render(request,"depart_list.html",{"queryset":queryset})
+
+def depart_add(request):
+    """添加部门"""
+    name=models.Department.objects.get("title")
+    models.Department.objects.create(title=name)
+    return render(request,"depart_add.html")
