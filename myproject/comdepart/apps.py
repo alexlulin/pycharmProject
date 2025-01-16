b68d881c1d5525296a1d97f44105acd77fd836ed
alexlulin232100 <7986202+alexlulin232100@user.noreply.gitee.com>
2025-01-16 14:55:33 +0800

    第一次commit

---
+++ myproject/comdepart/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class ComdepartConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'comdepart'
---
+++ myproject/comdepart/models.py
@@ -0,0 +1,20 @@
+from django.db import models
+
+# Create your models here.
+class Department(models.Model):
+    """部门表"""
+    title = models.CharField(verbose_name="标题",max_length=32)
+
+class UserInfo(models.Model):
+    """员工表"""
+    name=models.CharField(max_length=16,verbose_name="姓名")
+    password=models.CharField(max_length=64,verbose_name="密码")
+    age=models.IntegerField(verbose_name="年龄")
+    account=models.DecimalField(verbose_name="账户余额",decimal_places=2,default=0,max_digits=10)
+    create_time=models.DateTimeField(verbose_name="入职时间")
+    #depart_id = models.ForeignKey(verbose_name="部门ID",to=Department,to_field='id',on_delete=models.CASCADE)
+    gender_choices=(
+        (1,"男"),
+        (2,"女"),
+    )
+    gender=models.SmallIntegerField(verbose_name="性别",choices=gender_choices)
